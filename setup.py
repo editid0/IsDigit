0ab11dc6c0811e81475145e872dadf3327ebeb23
editid <slyfoxdiscordbot@gmail.com>
2021-09-05 22:37:30 +0200

    Examples, and setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,14 @@ setup(
     license='MIT',
     description='Simple, mainly useless python package that checks if a string, int, or float is a digit.',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+    keywords='python isdigit float int string'
 )
