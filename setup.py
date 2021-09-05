5042dc63a6757ef9285cf8a13494dcd509c3228a
editid <slyfoxdiscordbot@gmail.com>
2021-09-05 21:59:41 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='IsDigit',
+    url='https://github.com/editid0/IsDigit',
+    author='editid',
+    packages=['isdigit'],
+    version='0.1',
+    license='MIT',
+    description='Simple, mainly useless python package that checks if a string, int, or float is a digit.',
+    long_description=open('README.md').read(),
+)
